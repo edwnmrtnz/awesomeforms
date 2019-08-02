75e07765fdc5a4f7572a05ec373b24003c934d4a
raquezha <rarwkk@gmail.com>
2019-08-02 21:28:36 +0800

    wip spinner

--- app/src/main/java/com/github/edwnmrtnz/awesomeforms/MainActivity.kt
+++ app/src/main/java/com/github/edwnmrtnz/awesomeforms/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.edwnmrtnz.awesomeforms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        etFirstName.setError("Hello Error occured!")
+        etFirstName.setError("first name is required")
+        etLastName.setError("last name is required")
+        etNumber.setError("input a number")
+        etPassword.setError("password required")
+        etGender.setError("this field is required")
 
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.exposed_dropdown_popup_item,
+            resources.getStringArray(R.array.exposed_dropdown_content)
+        )
+
+        etGender.setAdapter(adapter)
     }
 }
