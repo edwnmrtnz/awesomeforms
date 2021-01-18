2a347317f842802b59f4575a7db90d95692509fd
raquezha <raquezha@gmail.com>
2021-01-18 17:33:13 +0800

    Fix emoji filtering returns ""

--- library/src/main/java/com/github/edwnmrtnz/awesomeforms/library/EmojiFilter.kt
+++ library/src/main/java/com/github/edwnmrtnz/awesomeforms/library/EmojiFilter.kt
@@ -6,11 +6,11 @@ import android.text.InputFilter
 object EmojiFilter {
     val filter: Array<InputFilter>
         get() {
-            val emojiFilter = InputFilter { source, start, end, _, _, _ ->
+            val emojiFilter = InputFilter { source, start, end, _, dStart, dEnd ->
                 for (index in start until end) {
                     val type = Character.getType(source[index])
                     if (type == Character.SURROGATE.toInt() || type == Character.NON_SPACING_MARK.toInt() || type == Character.OTHER_SYMBOL.toInt()) {
-                        return@InputFilter ""
+                        return@InputFilter "${source.subSequence(dStart, dEnd)}"
                     }
                 }
                 null
